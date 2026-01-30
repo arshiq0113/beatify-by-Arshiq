ffdf7a0e9bcaa368fae56a9699a7f746ac90dd9e
arshiq0113 <znaaz6556@gmail.com>
2026-01-31 02:47:43 +0530

    Add files via upload

---
+++ 1st.js
@@ -0,0 +1,149 @@
+/*const student ={
+    fullName : "Arshiq",
+    age : 17,
+    cgpa : 8.3,
+    ispass : true
+
+};
+student.age = student.age + 20;
+console.log(student.age);
+PRACTCE QUSTION ONE STARTS FROM HERE 
+*/
+/*
+const product =
+{
+    name : "Parker jotter Standard Ct  Ball pen (Black)",
+    rating : 4.3,
+    isdeal : true,
+    price : 270,
+    offer : "5% off"
+};
+console.log(product);
+OQUESTION COMPLETE HERE IN FIRST TRY 
+*/
+/*
+const profile =
+{
+    userName : "Arshiq",
+    isfollow : true,
+    follwers : 150,
+    following : 200,
+    post : 50,
+    bio  : "everyone seen profile but no one knows the real me"
+
+};
+console.log(typeof profile.bio)
+so they give string as output
+2 PRACTICE QUESTION COMPLETE HERE IN FIRST TRY WITHOUT ANAY ERRROR 
+*/
+// let a = 2;
+// let b = 10;
+// let c = a + b;
+// let d = a * c;
+// let e = b - a;
+// console.log("a ** b = " , a ** b);
+/* IN CASE OF MODULAS EX 
+LET A = 50;
+LET B = 7;
+50 DIVIDED BY 7 = 7*7 = 49
+50 - 49 = 1 REMAINDER
+SO OUTPUT WILL BE 1 THIS IS HOW MODULAS WORKS
+*/
+/* EXPONENTIATION OPERATOR
+LET A = 2;
+LET B = 3;
+CONSOLE.LOG (A ** B);
+2*2*2 = 8 */
+/* INCREMENT OPERATOR
+LET A = 5;
+A++;
+console.log(A);
+//   let a = 10;*/
+//  a -- ;
+//  console.log("a -- = ", a);
+//  a = a--;
+//  console.log("a = a-- = ", a);
+// urinary increment and decrement operator
+// post increment a++
+//  let a = 5;
+//  a++;
+//  console.log("a++ = ", a);
+//  output will be 6
+// postincrement ++a
+// let a = 5;
+//  a++; 
+//     console.log("++a = ", a);// for better understanding we can doing  in a diffrent way
+    // let a = 5; 
+    // console.log("a++ = ", a++);
+    // console.log("a = ", a);
+    // output will be 5 and 6
+    //post decrement like post increment  2nd way beter understanding method 
+    //  let a = 5;
+    // console.log("a-- = ", a--);
+    // console.log("a = ", a);
+    // output will be 5 and 4
+    // ASSIGNMENT OPERATOR
+    //  let a = 2;
+    //  a += 5; // a = a + 5
+    // console.log("a += 5 = ", a);
+    // a -= 3; // a = a - 3
+    // comparison operator
+    //  let a = 5;
+    // let b = 10;
+    // console.log("a  == b ",  a == b); // false
+// === check number and type  of datatype also !==
+// let a = 5;
+// let b = "5";
+// console.log("a === b ",  a === b); // false
+// console.log("a !== b ",  a !== b); // true
+// let a = 10;
+// let b = 5;
+// console.log("a > b ",  a > b); // true
+// console.log("a < b ",  a < b); // false
+// console.log("a > b == a < b ",  a > b == (a < b)); // false
+// LOGICAL OPERATOR
+// let a = 10;
+// let b = 5;  
+// let code1 = a > b;// true
+// let code2 = a === 10;// true
+// // AND OPERATOR
+// console.log("code1 && code2 ", code1 && code2); // true 
+/*
+|  condition1 | condition2 | output |
+|-------------|-------------|--------|
+|    true     |    true     |  true  |
+|    true     |   false     | false  |
+|   false     |    true     | false  |
+|   false     |   false     | false  |    
+dono true hone par hi output true ayega  
+*/
+// // OR OPERATOR
+// let a = 10;
+// let b = 5;  
+//  let code1 = a > b;// true
+//  let code2 = a !== 10;// true
+// console.log("code1 || code2 ", code1 || code2); // true
+/*
+|  condition1 | condition2 | output |
+|-------------|-------------|--------|
+|    true     |    true     |  true  |
+|    true     |   false     |  true  |
+|   false     |    true     |  true  |
+|   false     |   false     | false  |  
+ek bhi true to output true ayega    
+*/  
+// console.log(3+false); // 3 + 0 = 3
+// console.log(3+true); // 3 + 1 = 4
+///IMPORTANT CONCEPT
+// console.log(true + false); // 1 + 0 = 1
+// console.log(true + true); // 1 + 1 = 2
+// console.log(false + false); // 0 + 0 = 0 
+// LOGICAL NOT OPERATOR
+// let a = 10;
+// let b = 5;
+// let code1 = a > b;// true
+// console.log("!code1 ", !code1); // false
+/*| condition |   !condition  |
+|-----------|---------------|
+|   true    |     false     |
+|  false    |     true      |*/ 
